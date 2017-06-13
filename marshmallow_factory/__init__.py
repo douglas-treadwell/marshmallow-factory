f11919de1f30784942eb39d68a4bae368dbe22fd
Douglas Treadwell <douglas.treadwell@gmail.com>
2017-06-12 23:09:21 -0700

    Adding setup files, tox.ini, license, and __version__.

--- marshmallow_factory/__init__.py
+++ marshmallow_factory/__init__.py
@@ -3,12 +3,16 @@ from marshmallow.schema import SchemaMeta
 from marshmallow.fields import Nested
 
 
+__version__ = '0.1.0'
+
+
 def schema(_schema_dict=None, **kwargs):
     _schema_dict = dict(_schema_dict or kwargs)  # create copy for changes
 
     for key, value in _schema_dict.items():
         if isinstance(value, dict):
             _schema_dict[key] = nested_schema(value)
+
     # Passing empty string prevents class from getting added
     # to marshmallow's class registry, thus saving memory
     return SchemaMeta(str(''), (Schema,), _schema_dict)
